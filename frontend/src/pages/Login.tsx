fa23df75f0be4cbacc4b28ec027f2449324a9a4b
b-sandrine <sanrdushimimana@gmail.com>
2023-03-09 13:28:48 +0200

    Forms & add bootstrap

--- frontend/src/pages/Login.tsx
+++ frontend/src/pages/Login.tsx
@@ -1,8 +1,21 @@
 
 export default function Login() {
     return (
-        <div className="login--container">
-            <h1>Login page</h1>
+        <div className="login--container w-50 mx-auto">
+            <h1 className="text-center m-5">Login page</h1>
+            <form action="">
+                <div className="form-group m-3">
+                    <label htmlFor="email" className="mb-1">Email</label>
+                    <input type="text" className="form-control" id="email" placeholder="Example : Joe-doe@gmail.com" name="email" />
+                </div>
+                <div className="form-group m-3">
+                    <label htmlFor="password" className="mb-1">Password</label>
+                    <input type="text" className="form-control" id="password" name="password" placeholder="Password" />
+                </div>
+                <div className="m-3">
+                    <input type="submit" name="" id="" value="Log In" className="btn btn-primary btn-lg"/>
+                </div>
+            </form>
         </div>
     )
 }
--- frontend/src/pages/Register.tsx
+++ frontend/src/pages/Register.tsx
@@ -1,8 +1,25 @@
 
 export default function Register() {
     return (
-        <div className="register--container">
-            <h1>Register page</h1>
+        <div className="register--container w-50 mx-auto">
+            <h1 className="text-center m-5">Login page</h1>
+            <form action="">
+                <div className="form-group m-3">
+                    <label htmlFor="name" className="mb-1">Names</label>
+                    <input type="text" className="form-control" id="name" placeholder="Example : Joe Doe" name="name" />
+                </div>
+                <div className="form-group m-3">
+                    <label htmlFor="email" className="mb-1">Email</label>
+                    <input type="text" className="form-control" id="email" placeholder="Example : Joe-doe@gmail.com" name="email" />
+                </div>
+                <div className="form-group m-3">
+                    <label htmlFor="password" className="mb-1">Password</label>
+                    <input type="text" className="form-control" id="password" name="password" placeholder="Password" />
+                </div>
+                <div className="m-3">
+                    <input type="submit" name="" id="" value="Register" className="btn btn-primary btn-lg"/>
+                </div>
+            </form>
         </div>
     )
 }
