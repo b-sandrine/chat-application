9b4c1772ee03c776f015364d716fd14fe137e9b4
b-sandrine <sanrdushimimana@gmail.com>
2023-03-10 11:20:39 +0200

    Installed react-icons & chat page

--- frontend/src/pages/Chat.tsx
+++ frontend/src/pages/Chat.tsx
@@ -1,9 +1,77 @@
-import Navbar from "../components/Navbar"
-
+import profile from '../assets/image-2.avif'
+import { MdNotificationImportant, MdOutlineMessage, MdSettings, MdPeopleAlt, MdEditDocument } from 'react-icons/md'
+import { MdArrowCircleDown, MdSearch } from 'react-icons/md'
 export default function Chat () {
     return (
         <div className="chat--container">
-            <Navbar />
+            <div className="left--sidebar">
+                <div className="left--sidebar--profile">
+                    <img src={profile} alt="Profile image" />
+                    <p>user name</p>
+                </div>
+                <div className="left--sidebar--menu">
+                    <div className="menu--holder">
+                        <div className="single--item">
+                            <div className="icon">
+                                <MdOutlineMessage />
+                            </div>
+                            <div className="name">Chats</div>
+                        </div>
+                        <div className="single--item">
+                            <div className="icon">
+                                <MdNotificationImportant />
+                            </div>
+                            <div className="name">Notifications</div>
+                        </div>
+                        <div className="single--item">
+                            <div className="icon">
+                                <MdSettings />
+                            </div>
+                            <div className="name">Settings</div>
+                        </div>
+                        <div className="single--item">
+                            <div className="icon">
+                                <MdPeopleAlt />
+                            </div>
+                            <div className="name">Friends</div>
+                        </div>
+                        <div className="single--item">
+                            <div className="icon">
+                                <MdEditDocument />
+                            </div>
+                            <div className="name">Documents</div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div className="right--sidebar">
+                <div className="right--sidebar--heading">
+                    <h5 className="heading">CHATS</h5>
+                    <div className="works">
+                        <div className="chat">
+                            <div className="holder">
+                                <h5>Recent Chats</h5>
+                                <span className="icon">
+                                    <MdArrowCircleDown />
+                                </span>
+                            </div>
+                            <button>+ New Chat</button>
+                        </div>
+                        <div className="search">
+                            <div className="icon">
+                                <MdSearch />
+                            </div>
+                            <input type="text" name="" id="" placeholder='Search'/>
+                        </div>
+                    </div>
+                </div>
+                <div className="right--sidebar--bottom">
+
+                </div>
+            </div>
+            <div className="chat--container">
+                chat page
+            </div>
         </div>
     )
 }
